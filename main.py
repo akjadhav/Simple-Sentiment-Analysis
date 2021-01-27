2ade9b72a1d3fdfc4e69f9bb777c50f33d898461
Ameya Jadhav <jadhavameyak@gmail.com>
2021-01-26 19:39:23 -0500

    Add files via upload
    added main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+sid = SentimentIntensityAnalyzer()
+
+title = input('Enter a sentence for Sentiment Analyzation: ')
+print('\n')
+
+global positivecounter
+global negativecounter
+global neutralcounter
+
+neg = 0.000
+pos = 0.000
+neu = 0.000
+negposneu = []
+
+ss = sid.polarity_scores(title)
+values = ss
+
+neg = float(values['neg'])
+neu = float(values['neu'])
+pos = float(values['pos'])
+
+negposneu.append(neg)
+negposneu.append(neu)
+negposneu.append(pos)
+
+neg = neg * 100.0
+neu = neu * 100.0
+pos = pos * 100.0
+
+print(title)
+print('Positive: ', pos, '%')
+print('Neutral: ', neu, '%')
+print('Negative: ', neg, '%')
+print(' ')
